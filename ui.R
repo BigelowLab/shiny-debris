200cc49f3bdcc4244d2e46306b74c4d6c5c07ca1
btupper <btupper@bigelow.org>
2018-11-13 09:19:35 -0500

    add analytics

--- ui.R
+++ ui.R
@@ -2,6 +2,7 @@ source("globals.R")
 
 ui <- bootstrapPage(
     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+    tags$head(includeScript("google-analytics.js")),
     leaflet::leafletOutput("map", width = "100%", height = "100%"),
     shiny::titlePanel("under development",
                      windowTitle = 'Debris Encounter Forecast'),
