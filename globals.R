ceb7befac07c38f3ebf6c82ea15f1e828fee6764
btupper <btupper@bigelow.org>
2018-11-14 15:32:15 -0500

    switch to raster::projectRaster from leaflet::projectRasterForLeaflet

--- globals.R
+++ globals.R
@@ -91,7 +91,7 @@ read_xcast <- function(dt = as.Date("2018-11-13"),
         x <- raster::setZ(x, dt)
     }
     if (!is.null(x) && !is.na(threshold)) x[TOPO <= threshold] <- NA
-    if (!is.null(x)) x <- raster::projectRaster(x,
+    if (!is.null(x)) x <- raster::projectRaster(x, method = 'bilinear',
         crs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs')
     x
 }
